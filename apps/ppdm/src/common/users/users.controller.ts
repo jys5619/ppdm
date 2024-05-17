ab418fb5efcb2c8c8e7a5712bfb5f47389c26aef
DESKTOP-8KMHP2P\JYS <noriks@nate.com>
2024-05-17 17:44:23 +0900

    PPDM-SERVER로 옮길예정

--- apps/ppdm/src/common/users/users.controller.ts
+++ apps/ppdm/src/common/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { PpdmPublicAuth } from '../../share/decorator';
 @Controller('users')
 @ApiTags('Users Controller')
 @ApiBearerAuth('access-token')
@@ -10,5 +11,11 @@ export class UsersController {
   @Get()
   findAll() {
     return this.usersService.findAll();
+  }
+
+  @Get('test')
+  @PpdmPublicAuth()
+  find1() {
+    return '안녕하세요.';
   }
 }
--- apps/ppdm/src/main.ts
+++ apps/ppdm/src/main.ts
@@ -5,6 +5,7 @@ import { SwaggerSetup } from './share/config/swagger.setup';
 async function bootstrap() {
   const app = await NestFactory.create(PpdmModule);
 
+  app.setGlobalPrefix('api');
   SwaggerSetup(app);
 
   await app.listen(3000);
